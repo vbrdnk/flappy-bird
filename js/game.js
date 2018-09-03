1e5898753f8bf019381299e1b1eccd8d58874161
vbrdnk <burdenyuchok@gmail.com>
2018-09-03 17:51:49 +0300

    add comments

--- js/game.js
+++ js/game.js
@@ -7,30 +7,34 @@ const foreGround = new Image();
 const pipeUp = new Image();
 const pipeBottom = new Image();
 
+// Создаем все обьекты для анимации
 bird.src = 'img/flappy_bird_bird.png';
 bg.src = 'img/flappy_bird_bg.png';
 foreGround.src = 'img/flappy_bird_fg.png';
 pipeUp.src = 'img/flappy_bird_pipeUp.png';
 pipeBottom.src = 'img/flappy_bird_pipeBottom.png';
 
-// Audio files
+// Аудио
 let fly = new Audio();
 let score_audio = new Audio();
 
 fly.src = 'audio/fly.mp3';
 score_audio.src = 'audio/score.mp3';
 
+
+// отступ между препятствиями
 let gap = 90;
 
 
-// click on any button
+// слушаем нажатие на любую клавишу
 document.addEventListener('keydown', moveUp);
 
 function moveUp() {
     yPosition -= 20;
+    fly.play();
 }
 
-// create new blocks
+// блоки препятствий
 let pipe = [];
 
 pipe[0] = {
@@ -39,12 +43,12 @@ pipe[0] = {
 }
 
 let score = 0;
-// bird's position
+// позиция птички
 let xPosition = 10;
 let yPosition = 150;
 let gravity = 1;
 
-
+// рисуем все елементы в канвасе
 function draw() {
     context.drawImage(bg, 0, 0);
 
@@ -54,6 +58,7 @@ function draw() {
 
         pipe[i].x--;
 
+        // добавляем новые препятствия
         if (pipe[i].x === 125) {
             pipe.push({
                 x: canvas.width,
@@ -61,7 +66,7 @@ function draw() {
             })
         }
 
-        // Check keybord touches
+        // Проверяем или птичка находится в пределах ширины препятствие и находится ли она в пределах его высоты
         if( xPosition + bird.width >= pipe[i].x
             && xPosition <= pipe[i].x + pipeUp.width
             && (yPosition <= pipe[i].y + pipeUp.height
@@ -69,6 +74,7 @@ function draw() {
                 location.reload(); // reload the page
             }
         
+            // если препятствие пройдено и уже вначале нашего фрейма увеличиваем счет
             if (pipe[i].x == 5) {
                 score++;
                 score_audio.play();
